162b60a8474456a4dc049718a84f0324b5b175d4
Harsh_it <harshitsingh562006@gmail.com>
2026-07-26 01:15:48 +0530

    Add funct3 input/output to ex_mem module

--- Source Files/ex_mem.v
+++ Source Files/ex_mem.v
@@ -4,6 +4,7 @@ module ex_mem_reg (
     input  wire [31:0] alu_result_in,
     input  wire [31:0] rs2_in,
     input  wire [4:0]  rd_in,
+    input  wire [2:0]  funct3_in,
     input  wire        MemRead_in,
     input  wire        MemWrite_in,
     input  wire        RegWrite_in,
@@ -11,6 +12,7 @@ module ex_mem_reg (
     output reg  [31:0] alu_result_out,
     output reg  [31:0] rs2_out,
     output reg  [4:0]  rd_out,
+    output reg  [2:0]  funct3_out,
     output reg         MemRead_out,
     output reg         MemWrite_out,
     output reg         RegWrite_out,
@@ -21,6 +23,7 @@ module ex_mem_reg (
             alu_result_out <= 32'd0;
             rs2_out        <= 32'd0;
             rd_out         <= 5'd0;
+            funct3_out     <= 3'd0;
             MemRead_out    <= 1'b0;
             MemWrite_out   <= 1'b0;
             RegWrite_out   <= 1'b0;
@@ -29,6 +32,7 @@ module ex_mem_reg (
             alu_result_out <= alu_result_in;
             rs2_out        <= rs2_in;
             rd_out         <= rd_in;
+            funct3_out     <= funct3_in;
             MemRead_out    <= MemRead_in;
             MemWrite_out   <= MemWrite_in;
             RegWrite_out   <= RegWrite_in;
