ff52d9db3b22a5345467b9131e930577b60475a2
Harsh_it <harshitsingh562006@gmail.com>
2026-07-29 04:07:13 +0530

    Update control_unit.v

--- Source Files/control_unit.v
+++ Source Files/control_unit.v
@@ -7,14 +7,14 @@ module control_unit (
     output reg [1:0]  ALUOp
 );
     always @(*) begin
-        // Default control signals = 0
+        
         MemtoReg = 0; RegWrite = 0; MemRead = 0; MemWrite = 0;
         ALUSrc = 0; Branch = 0; Jump = 0; Jalr = 0; ALUOp = 2'b00;
         case (opcode)
             7'b0110011: begin // R-type
                 RegWrite = 1; ALUOp = 2'b10;
             end
-            7'b0010011: begin // I-type ALU (ADDI, SLTI, etc.)
+            7'b0010011: begin // I-type ALU 
                 RegWrite = 1; ALUSrc = 1; ALUOp = 2'b10;
             end
             7'b0000011: begin // Load (LW)
@@ -39,7 +39,7 @@ module control_unit (
                 RegWrite = 1; ALUSrc = 1; ALUOp = 2'b10;
             end
             default: begin
-                // unsupported opcode: all signals remain 0
+               
             end
         endcase
     end
