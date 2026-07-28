1c36b94338d7eaff57145f4de2fb34c8a7b382d8
Harsh_it <harshitsingh562006@gmail.com>
2026-07-29 04:06:36 +0530

    Update alu_control.v

--- Source Files/alu_control.v
+++ Source Files/alu_control.v
@@ -10,14 +10,14 @@ module alu_control (
             2'b01: ALU_control = 4'b0001; // SUB (for branches)
             2'b10: begin
                 case (funct3)
-                    3'b000: ALU_control = funct7_5 ? 4'b0001 : 4'b0000; // SUB/ADD
-                    3'b001: ALU_control = 4'b0101; // SLL
-                    3'b010: ALU_control = 4'b1000; // SLT
-                    3'b011: ALU_control = 4'b1001; // SLTU
-                    3'b100: ALU_control = 4'b0100; // XOR
-                    3'b101: ALU_control = funct7_5 ? 4'b0111 : 4'b0110; // SRA/SRL
-                    3'b110: ALU_control = 4'b0011; // OR
-                    3'b111: ALU_control = 4'b0010; // AND
+                    3'b000: ALU_control = funct7_5 ? 4'b0001 : 4'b0000;  // SUB/ADD
+                    3'b001: ALU_control = 4'b0101;                       // SLL
+                    3'b010: ALU_control = 4'b1000;                       // SLT
+                    3'b011: ALU_control = 4'b1001;                       // SLTU
+                    3'b100: ALU_control = 4'b0100;                       // XOR
+                    3'b101: ALU_control = funct7_5 ? 4'b0111 : 4'b0110;  // SRA/SRL
+                    3'b110: ALU_control = 4'b0011;                       // OR
+                    3'b111: ALU_control = 4'b0010;                       // AND
                     default: ALU_control = 4'b0000;
                 endcase
             end
